014482f1c85bd73071257e8347d9ee2522968e45
Idriss-Mikdame <mikdameidriss420@gmail.com>
2025-04-20 16:21:49 +0100

    quelque  modification1

---
+++ src/app/components/resoult/resoult.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resoult',
+  imports: [],
+  templateUrl: './resoult.component.html',
+  styleUrl: './resoult.component.scss'
+})
+export class ResoultComponent {
+
+}
